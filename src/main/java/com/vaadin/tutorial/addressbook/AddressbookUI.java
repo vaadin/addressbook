56986e1b25c3b4367a31e0d17861449c92573265
Matti Tahvonen <matti@vaadin.com>
2015-03-11 08:19:25 +0200

    some documentation and cleanup

--- src/main/java/com/vaadin/tutorial/addressbook/AddressbookUI.java
+++ src/main/java/com/vaadin/tutorial/addressbook/AddressbookUI.java
@@ -41,51 +41,52 @@ public class AddressbookUI extends UI {
     public static class MyUIServlet extends VaadinServlet {
     }
 
+    /* ContactService mimics a real world DAO, that you'd typically implement
+     * as EJB or Spring Data based service.
+     */
     private ContactService service = ContactService.createDemoService();
 
     private TextField filter = new TextField();
-    private Button newContact = new Button("New contact",
-            new Button.ClickListener() {
-
-                @Override
-                public void buttonClick(ClickEvent event) {
-                    contactForm.edit(new Contact());
-                }
-            });
+    private Button newContact = new Button("New contact");
 
     private Table contactList = new Table();
 
     private ContactForm contactForm = new ContactForm(this);
 
+    /**
+     * The UI.init is the "public static void main(String... args)" for your
+     * Vaadin application. It is the entry point method executed for each user
+     * who enters your application.
+     *
+     * @param request the request object contains some low level web request
+     * data that you can typically ignore.
+     */
     @Override
     protected void init(VaadinRequest request) {
-        // Configure components
+        // Configure components and wire logic to them
+        newContact.addClickListener(new Button.ClickListener() {
+            @Override
+            public void buttonClick(ClickEvent event) {
+                editContact(new Contact());
+            }
+        });
+
         filter.setInputPrompt("Filter contacts...");
         filter.addTextChangeListener(new FieldEvents.TextChangeListener() {
-
             @Override
             public void textChange(FieldEvents.TextChangeEvent event) {
                 listContacts(event.getText());
             }
-
         });
-
-        contactForm.setVisible(false);
 
         contactList.setSelectable(true);
         contactList.addValueChangeListener(new Property.ValueChangeListener() {
-
             @Override
             public void valueChange(Property.ValueChangeEvent event) {
-                Contact contact = (Contact) event.getProperty().getValue();
-                if (contact != null) {
-                    contactForm.edit(contact);
-                } else {
-                    contactForm.setVisible(false);
-                }
+                editContact((Contact) event.getProperty().getValue());
             }
         });
-        
+
         // Build main layout
         HorizontalLayout actions = new HorizontalLayout(filter, newContact);
         actions.setWidth("100%");
@@ -115,12 +116,23 @@ public class AddressbookUI extends UI {
         contactForm.setVisible(false);
     }
 
-    void save(Contact contact) {
+    private void editContact(Contact contact) {
+        if (contact != null) {
+            contactForm.edit(contact);
+        } else {
+            contactForm.setVisible(false);
+        }
+    }
+
+    /* These methods are public and are called by ContactForm when user
+     * wants to persist or reset changes to the edited contact.
+     */
+    public void save(Contact contact) {
         service.save(contact);
         listContacts();
     }
 
-    void deselect() {
+    public void deselect() {
         contactList.setValue(null);
     }
 

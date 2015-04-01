a8b6222f35395691176e7cf8050d3adf645b19a6
Matti Tahvonen <matti@vaadin.com>
2015-04-01 12:38:37 +0300

    Resolve merge

--- src/main/java/com/vaadin/tutorial/addressbook/AddressbookUI.java
+++ src/main/java/com/vaadin/tutorial/addressbook/AddressbookUI.java
@@ -1,7 +1,137 @@
 package com.vaadin.tutorial.addressbook;
 
-/**
- * Created by se on 13/04/15.
+import com.vaadin.annotations.Theme;
+import com.vaadin.annotations.Title;
+import com.vaadin.annotations.VaadinServletConfiguration;
+import com.vaadin.data.Property;
+import com.vaadin.data.util.BeanItemContainer;
+import com.vaadin.event.FieldEvents;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.server.VaadinServlet;
+import com.vaadin.tutorial.addressbook.backend.Contact;
+import com.vaadin.tutorial.addressbook.backend.ContactService;
+import com.vaadin.ui.*;
+import com.vaadin.ui.Button.ClickEvent;
+
+import javax.servlet.annotation.WebServlet;
+import java.util.Arrays;
+
+/* 
+ * UI class is the starting point for your app. You may deploy it with VaadinServlet
+ * or VaadinPortlet by giving your UI class name a parameter. When you browse to your
+ * app a web page showing your UI is automatically generated. Or you may choose to 
+ * embed your UI to an existing web page. 
  */
-public class AddressbookUI {
+@Title("Addressbook")
+@Theme("valo")
+public class AddressbookUI extends UI {
+
+	/**
+	 * Vaadin applications are basically just Serlvlets, so lets define one with
+	 * Servlet 3.0 style. Naturally you can use the plain old web.xml file as
+	 * well.
+	 */
+	@WebServlet(urlPatterns = "/*")
+	@VaadinServletConfiguration(ui = AddressbookUI.class, productionMode = false)
+	public static class MyUIServlet extends VaadinServlet {
+	}
+
+	/*
+	 * ContactService mimics a real world DAO, that you'd typically implement as
+	 * EJB or Spring Data based service.
+	 */
+	private ContactService service = ContactService.createDemoService();
+
+	private TextField filter = new TextField();
+	private Button newContact = new Button("New contact");
+
+	private Table contactList = new Table();
+
+	private ContactForm contactForm = new ContactForm(this);
+
+	/**
+	 * The UI.init is the "public static void main(String... args)" for your
+	 * Vaadin application. It is the entry point method executed for each user
+	 * who enters your application.
+	 *
+	 * @param request
+	 *            the request object contains some low level web request data
+	 *            that you can typically ignore.
+	 */
+	@Override
+	protected void init(VaadinRequest request) {
+		// Configure components and wire logic to them
+		newContact.addClickListener(new Button.ClickListener() {
+			@Override
+			public void buttonClick(ClickEvent event) {
+				editContact(new Contact());
+			}
+		});
+
+		filter.setInputPrompt("Filter contacts...");
+		filter.addTextChangeListener(new FieldEvents.TextChangeListener() {
+			@Override
+			public void textChange(FieldEvents.TextChangeEvent event) {
+				listContacts(event.getText());
+			}
+		});
+
+		contactList.setSelectable(true);
+		contactList.addValueChangeListener(new Property.ValueChangeListener() {
+			@Override
+			public void valueChange(Property.ValueChangeEvent event) {
+				editContact((Contact) event.getProperty().getValue());
+			}
+		});
+
+		// Build main layout
+		HorizontalLayout actions = new HorizontalLayout(filter, newContact);
+		actions.setWidth("100%");
+		filter.setWidth("100%");
+		actions.setExpandRatio(filter, 1);
+
+		VerticalLayout left = new VerticalLayout(actions, contactList);
+		left.setSizeFull();
+		contactList.setSizeFull();
+		left.setExpandRatio(contactList, 1);
+
+		setContent(new HorizontalSplitPanel(left, contactForm));
+
+		// List initial content from the "backend"
+		listContacts();
+	}
+
+	private void listContacts() {
+		listContacts(filter.getValue());
+	}
+
+	private void listContacts(String text) {
+		contactList.setContainerDataSource(new BeanItemContainer<>(
+				Contact.class, service.findAll(text)), Arrays.asList(
+				"firstName", "lastName", "email"));
+		contactList.setColumnHeaders("First name", "Last name", "email");
+		contactForm.setVisible(false);
+	}
+
+	private void editContact(Contact contact) {
+		if (contact != null) {
+			contactForm.edit(contact);
+		} else {
+			contactForm.setVisible(false);
+		}
+	}
+
+	/*
+	 * These methods are public and are called by ContactForm when user wants to
+	 * persist or reset changes to the edited contact.
+	 */
+	public void save(Contact contact) {
+		service.save(contact);
+		listContacts();
+	}
+
+	public void deselect() {
+		contactList.setValue(null);
+	}
+
 }
--- src/main/java/com/vaadin/tutorial/addressbook/ContactForm.java
+++ src/main/java/com/vaadin/tutorial/addressbook/ContactForm.java
@@ -1,10 +1,16 @@
 package com.vaadin.tutorial.addressbook;
 
 import com.vaadin.data.fieldgroup.BeanFieldGroup;
+import com.vaadin.event.ShortcutAction;
+import com.vaadin.shared.ui.MarginInfo;
 import com.vaadin.tutorial.addressbook.backend.Contact;
 import com.vaadin.ui.Button;
-import com.vaadin.ui.Button.ClickEvent;
-import com.vaadin.ui.Button.ClickListener;
+import com.vaadin.ui.DateField;
+import com.vaadin.ui.FormLayout;
+import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.TextField;
+import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.themes.ValoTheme;
 
 /**
  * The part of the UI modifying a Contact instance.
@@ -15,26 +21,55 @@ import com.vaadin.ui.Button.ClickListener;
  * You could also auto-generate the fields at runtime using bean introspection,
  * but typically this is not as flexible as defining then in your UI code.
  */
-public class ContactForm extends ContactFormLayout {
+public class ContactForm extends VerticalLayout implements Button.ClickListener {
+
+	private Button save = new Button("Save", this);
+	private Button cancel = new Button("Cancel", this);
+
+	private TextField firstName = new TextField("First name");
+	private TextField lastName = new TextField("Last name");
+	private TextField phone = new TextField("Phone");
+	private TextField email = new TextField("Email");
+	private DateField birthDate = new DateField("Birth date");
 
 	private final AddressbookUI mainUI;
 	private Contact contact;
 
-	public ContactForm(AddressbookUI addressbookUID) {
-		this.mainUI = addressbookUID;
-		save.addClickListener(new Button.ClickListener() {
-			@Override
-			public void buttonClick(ClickEvent event) {
-				mainUI.save(contact);
-			}
-		});
-		cancel.addClickListener(new ClickListener() {
+	public ContactForm(AddressbookUI mainUI) {
+		this.mainUI = mainUI;
+		setVisible(false);
 
-			@Override
-			public void buttonClick(ClickEvent event) {
-				mainUI.deselect();
-			}
-		});
+		final HorizontalLayout actions = new HorizontalLayout(save, cancel);
+		actions.setSpacing(true);
+
+		addComponent(new FormLayout(actions, firstName, lastName, phone, email,
+				birthDate));
+		setMargin(new MarginInfo(false, true, false, true));
+
+		/*
+		 * The core theme has lots of handy styles for components, here we make
+		 * the save button stand out from other buttons and give it a keyboard
+		 * shortcut to give a better UX
+		 */
+		save.setStyleName(ValoTheme.BUTTON_PRIMARY);
+		save.setClickShortcut(ShortcutAction.KeyCode.ENTER);
+	}
+
+	/**
+	 * Instead of using inner class for event listeners like we did in
+	 * AddressBookUI, a common practice is to implement listeners in your
+	 * compositions or in separate controller classes. Clicking save or cancel
+	 * in this application just passes the control back to the main view.
+	 *
+	 * @param event
+	 */
+	@Override
+	public void buttonClick(Button.ClickEvent event) {
+		if (event.getButton() == save) {
+			mainUI.save(contact);
+		} else if (event.getButton() == cancel) {
+			mainUI.deselect();
+		}
 	}
 
 	public void edit(Contact contact) {

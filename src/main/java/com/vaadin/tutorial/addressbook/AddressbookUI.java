792f4bd572c8820c1c0b8479295d1db771e450ee
ajsteadly <ajsteadly@users.noreply.github.com>
2017-02-03 16:10:32 -0400

    Update and rename ToDoListUI.java to AddressbookUI.java

--- src/main/java/com/vaadin/tutorial/addressbook/AddressbookUI.java
+++ src/main/java/com/vaadin/tutorial/addressbook/AddressbookUI.java
@@ -29,7 +29,7 @@ import com.vaadin.v7.ui.TextField;
 @Title("Addressbook")
 @Theme("valo")
 @Widgetset("com.vaadin.v7.Vaadin7WidgetSet")
-public class ToDoListUI extends UI {
+public class AddressbookUI extends UI {
 
     /*
      * Hundreds of widgets. Vaadin's user interface components are just Java

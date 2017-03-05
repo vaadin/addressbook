a191ec5e32dfc1a07a896dbb8cd9d1570a219958
Greydesk <rod.naugler@gmail.com>
2017-03-04 20:39:40 -0400

    Added EventService class for handling events with database backend

---
+++ src/main/java/com/vaadin/tutorial/addressbook/backend/EventService.java
@@ -0,0 +1,17 @@
+package com.vaadin.tutorial.addressbook.backend;
+
+import org.apache.commons.beanutils.BeanUtils;
+
+import java.util.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class EventService {
+
+}

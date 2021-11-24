8f542e991b79daf5d3496c5a3ed455d43dc966c0
UpShiftNow <usntechnologies@gmail.com>
2021-11-24 02:17:06 +0000

    add more names

--- src/main/java/com/vaadin/tutorial/addressbook/backend/ContactService.java
+++ src/main/java/com/vaadin/tutorial/addressbook/backend/ContactService.java
@@ -19,11 +19,11 @@ public class ContactService {
     static String[] fnames = { "Peter", "Alice", "John", "Mike", "Olivia",
             "Nina", "Alex", "Rita", "Dan", "Umberto", "Henrik", "Rene", "Lisa",
             "Linda", "Timothy", "Daniel", "Brian", "George", "Scott",
-            "Jennifer" };
+            "Jennifer", "Victor", "Amit", "Flavine" };
     static String[] lnames = { "Smith", "Johnson", "Williams", "Jones",
             "Brown", "Davis", "Miller", "Wilson", "Moore", "Taylor",
             "Anderson", "Thomas", "Jackson", "White", "Harris", "Martin",
-            "Thompson", "Young", "King", "Robinson" };
+            "Thompson", "Young", "King", "Robinson", "Kumar", "junior", "Harris" };
 
     private static ContactService instance;
 
